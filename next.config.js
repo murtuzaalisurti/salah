176d4599161e7c55bf835750387c40fb5f65ac5f
murtuzaalisurti <murtuza.creativity@gmail.com>
2025-02-15 12:10:27 +0530

    chore: upgrade deps

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa');
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
+const withPWA = require('next-pwa')({
+  dest: 'public',
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development"
-  }
-})
+});
+
+module.exports = withPWA();
